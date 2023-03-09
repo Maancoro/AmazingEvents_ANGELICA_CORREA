127d3eb751b948a678c566f2643b8beb7f491982
Angelica <correa_angelica@hotmail.com>
2023-03-09 16:41:01 -0300

    primer commit

---
+++ scripts/main.js
@@ -0,0 +1,25 @@
+import data from "./data.js"
+
+function agregarTodos(array) {
+  const cards = array.events.map((evento) => {
+    return `<div class="col">
+      <div class="card">
+        <img src=${evento.image} class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title"><b>${evento.name}</b></h5>
+          <p class="card-text"><b>${evento.date}</b></p>
+          <p class="card-text">${evento.description}</p>
+          <div class="footer-card d-flex">
+             <p><strong>Precio $${evento.price}</strong></p>
+            <a href="./details.html" class="btn btn-primary">ver mas</a>
+          </div>
+        </div>
+      </div>
+    </div>`;
+  });
+  
+  const cardSection = document.getElementById("cardSection");
+  cardSection.innerHTML = cards.join("");
+}
+
+agregarTodos(data);
